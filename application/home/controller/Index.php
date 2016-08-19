8e0debe100f3c2dd80586adea1ddf27e8d41c56b
liuxiqian <1454179583@qq.com>
2016-08-19 16:22:13 +0800

    修改

--- application/home/controller/Index.php
+++ application/home/controller/Index.php
@@ -10,38 +10,6 @@ class Index extends Controller
         return $this->fetch();
     }
 
-    public function flot()
-    {
-      return $this->fetch('template/flot');
-    }
 
-    public function forms()
-    {
-      return $this->fetch('template/forms');
-    }
 
-    public function buttons()
-    {
-      return $this->fetch('template/buttons');
-    }
-
-    public function icons()
-    {
-      return $this->fetch('template/icons');
-    }
-
-    public function notifications()
-    {
-      return $this->fetch('template/notifications');
-    }
-
-    public function tables()
-    {
-      return $this->fetch('template/tables');
-    }
-
-    public function typography()
-    {
-      return $this->fetch('template/typography');
-    }
 }
