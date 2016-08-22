26265d2eb6ea296ea791a45bcb9229556e26eec5
tangzhenjie <2819786276@qq.com>
2016-08-22 19:32:27 +0800

    user

--- application/model/User.php
+++ application/model/User.php
@@ -1,10 +1,44 @@
 <?php
 namespace app\model;
+
 use think\Model;
-/**
-* 用户类
-*/
+
 class User extends Model
 {
+	protected $dateFormat = 'Y/m/d';
+    protected $type       = [
+        // 设置last_time_on为时间戳类型（整型）
+        'last_time_on' => 'timestamp',
+    ];
+	static public function islogin()
+	{
+		$userId = session('userId');
+		if(isset($userId))
+		{
+			return true;
+		}else{
+			return false;
+		}
+	}
 
+	static public function login($username , $password)
+	{
+		$map = array('id' => $username);
+		$User = self::get($map);
+
+		//判断是否取出对应的对象
+		if($User === null)
+		{
+			return false;
+		}
+	    if($User->password === $password)
+		{
+		    session('userId' , $User->getData('id'));
+		    return true;
+		              
+	    }else{
+		    return false;
+	    }
+	
+	}
 }
