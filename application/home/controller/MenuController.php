afe6a778e734d67732bb46dbecda1ce401e2a70f
tangzhenjie <2819786276@qq.com>
2016-08-23 20:58:18 +0800

    菜单模块

--- application/home/controller/MenuController.php
+++ application/home/controller/MenuController.php
@@ -3,10 +3,20 @@ namespace app\home\controller;
 
 use app\home\controller\ParenterController;
 
+use app\model\Menu;
+
 class MenuController extends ParenterController
 {
 	public function index()
 	{
+		/**
+		*@tangzhenjie
+		*/
+		$name = input('get.name');
+        $pageSize = 2;
+        $Menu  = new Menu;
+        $Menus = $Menu->where('name', 'like', '%' . $name . '%')->paginate($pageSize);
+		$this->assign('Menus', $Menus);
 		return $this->fetch();
 	}
 
@@ -17,22 +27,63 @@ class MenuController extends ParenterController
     
     public function insert()
     {
-    	return $this->redirect("index");
+    	/**
+    	*@tangzhenjie
+    	*/
+    	$data = input('post.');
+    	$Menu = new Menu;
+    	if(false === $Menu->validate(true)->save($data))
+    	{
+    		return $this->error('添加失败', url('add'));
+    	}else{
+    		return $this->success('添加成功', url('index'));
+    	}
     }
 
 	public function edit()
 	{
+		$id = input('id/d');
+		$Menu = Menu::get($id);
+		$this->assign('Menu', $Menu);
 		return $this->fetch();
 	}
      
     public function update()
     {
-    	return $this->redirect('index');
-    }
+    	/**
+    	*@tangzhenjie
+    	*/
+    	$data = array(
+    		'name' => input('post.name'), 
+			'module' => input('post.module'), 
+			'controller' => input('post.controller'),
+			'action' => input('post.action'),
+			'weight' => input('post.weight'),
+			'status' => input('post.status'),
+			'is_permission' => input('post.is_permission')
+        );
+    	$Menu = new Menu;
+    	if(false === $Menu->validate(true)->save($data))
+    	{
+    		return $this->error('添加失败', url('add'));
+    	}else{
+    		return $this->success('添加成功', url('index'));
+    	}
+   }
 
     public function delete()
     {
-       return $this->redirect('index');
+    	/**
+    	*@tangzhenjie
+    	*/
+    	$id = input('id/d');
+    	$Menu = Menu::get($id);
+    	if($Menu->delete())
+    	{
+    		return $this->success('删除成功', url('index'));
+    	}else{
+    		return $this->error('删除失败', url('index'));
+    	}
     }
 
 }
