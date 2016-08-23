bbd39ffb7c818b651c58a349f605de1423372f4e
tangzhenjie <2819786276@qq.com>
2016-08-23 14:13:01 +0800

    类别管理改进和文章管理

--- application/home/controller/ArticleController.php
+++ application/home/controller/ArticleController.php
@@ -3,6 +3,9 @@ namespace app\home\controller;
 
 use app\home\controller\ParenterController;
 
+use app\model\Article;
+use app\model\Category;
+
 /**
 * @author liuxi
 */
@@ -10,21 +13,74 @@ class ArticleController extends ParenterController
 {
     public function index()
     {
-        //返回首页
+        /**
+        *@tangzhenjie
+        */
+        $title = input('post.title');
+        $page = input('page/d') < 1 ?1 : input('page/d');
+        $pageSize = 2;
+        $Article  = new Article;
+        $Articles = $Article->where('title', 'like', '%' . $title . '%')->paginate($pageSize);
+        $this->assign('Articles', $Articles);
         return $this->fetch();
     }
+
     public function edit()
     {
         //返回编辑的页面
+        $id = input('id/d');
+        $Article = Article::get($id);
+        $Category = new Category;
+        $Categorys = $Category->select();
+        $this->assign('Article', $Article);
+        $this->assign('Categorys', $Categorys);
         return $this->fetch();
     }
     
     public function update()
     {
-        return $this->redirect(url('index'));
+        $data = input('post.');
+        $id = input('post.id');
+        $Article = Article::get($id);
+        if(false === $Article->validate(true)->save($data))
+        {
+            return $this->error('更新失败', url('index'));
+        }else{
+            return $this->success('更新成功', url('index'));
+        }
     }
+
     public function delete()
     {
-        return $this->redirect(url('index'));
+        /**
+        *@tangzhenjie
+        */
+        $id = input('id/d');
+        $Article = Article::get($id);
+        if($Article->delete())
+        {
+            return $this->success('删除成功', url('index'));
+        }
     }
+
+    public function add()
+    {
+        $Category = new Category;
+        $Categorys = $Category->select();
+        $this->assign('Categorys', $Categorys);
+        return $this->fetch();
+    }
+
+    public function insert()
+    {
+        $data = input('post.');
+        $Article = new Article;
+        if(false === $Article->validate(true)->save($data))
+        {
+            return $this->error('添加失败'.$Article->getError(), url('add'));
+        }else{
+            return $this->success('添加成功'.$Article->getError(), url('index'));
+        }
+    }
+
 }
