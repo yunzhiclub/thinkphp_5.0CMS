689d8e98b96d04c8d18fabfef0e83c398333c33a
myquilt1101 <myquilt@126.com>
2016-08-26 20:27:03 +0800

    增加了一个文章列表的二级页面
    只是增加了页面，下一步写方法，并且按照点击量排序。

---
+++ application/index/controller/SubpageController.php
@@ -0,0 +1,16 @@
+<?php
+namespace app\index\controller;
+use think\Controller;
+
+class SubpageController extends Controller
+{
+	/**
+	 * @author liuyanzhao
+	 * 
+	 */
+    public function index()
+    {
+        return $this->fetch();
+    }
+    
+}
