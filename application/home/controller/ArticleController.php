2657a7445feffc134daea6457db5b1a998f71091
liuxiqian <1454179583@qq.com>
2016-08-27 19:35:07 +0800

    文章 修改图片路径

--- application/home/controller/ArticleController.php
+++ application/home/controller/ArticleController.php
@@ -104,8 +104,11 @@ class ArticleController extends ParenterController
 
         // 移动到框架应用根目录/public/uploads/ 目录下
         $info = $file->move(ROOT_PATH . 'public' . DS . 'images');
+        dump($info);
 
-        $savepath = $info->getRealPath();
+        $path = $info->getSaveName();
+        dump($path);
+        $savepath = '\public\images\\' . $path;
         $data = input('post.');
 
         if (input('post.is_top') === '1' && input('post.is_recomment') === '1') {
