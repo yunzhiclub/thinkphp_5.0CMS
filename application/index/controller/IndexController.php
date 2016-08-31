2817595828667648944590c780d27a2720f4dd76
gaoliming <xp01255410@icoud.com>
2016-08-31 08:33:31 +0800

    代码重构

--- application/index/controller/IndexController.php
+++ application/index/controller/IndexController.php
@@ -2,35 +2,27 @@
 namespace app\index\controller;
 use think\Controller;
 
-use app\index\controller\ParenterController;
+use app\index\controller\ParentController;
+
 use app\model\Article;
 use app\model\Systemset;
 
-class IndexController extends Controller
+class IndexController extends ParentController
 {
+    public function __construct()
+    {
+        //先执行父类的构造函数
+        parent::__construct();
+
+    }
     public function index()
     {
-        //取出点击量的前五条新闻
 		$New = new Article;
-		$News = $New->getMoreClickNum();
 
-        //取出置顶,推荐的文章
-        $TopNews = $New->getTopNews();
-
-        //取出首页的产品公司名称，页脚，log
-        $Systemset = new Systemset;
-        $Systemsets = $Systemset->getheader();
-        $Systemsetss = $Systemset->getfooter();
-        $Systemsetsss = $Systemset->getUrl();
         //取出slidershow的图片
         $SliderShows = $New->getSliderShow();
 
 		//向V层传值
-		$this->assign('News', $News);
-        $this->assign('TopNews', $TopNews);
-        $this->assign('Systemsets', $Systemsets);
-        $this->assign('Systemsetss', $Systemsetss);
-        $this->assign('Systemsetsss', $Systemsetsss);
         $this->assign('SliderShows', $SliderShows);
 
 		//返回首页
